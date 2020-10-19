d12078e86e594598d68dd9b54253ac569869fe95
mandareis <amanda@depaula.xyz>
2020-10-19 14:28:14 -0700

    Done.

--- solution.rb
+++ solution.rb
@@ -1,7 +1,33 @@
-class Deck
+require "pry"
 
+class Deck
+  attr_reader :cards
+
+  def initialize
+    @cards = []
+    suits = ["Hearts", "Clubs", "Diamonds", "Spades"]
+    ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    suits.each do |suit|
+      ranks.each do |rank|
+        @cards << Card.new(suit, rank)
+      end
+    end
+  end
+
+  def choose_card
+    idx = rand(@cards.length)
+    picked_card = @cards[idx]
+    @cards = @cards.select { |card| card != picked_card }
+    picked_card
+  end
 end
 
 class Card
+  attr_accessor :suit, :rank
 
+  def initialize(suit, rank)
+    @rank = rank
+    @suit = suit
+  end
 end
